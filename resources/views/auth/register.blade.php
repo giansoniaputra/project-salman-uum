cc098d20187b53f9d8ec9b890604511a456f5492
giansoniaputra <giansonia555@gmail.com>
2023-05-01 16:17:31 +0700

    Menambah Fitur Login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Sistem Informasi SMAC</title>
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="./images/favicon.png">
-    <link href="./css/style.css" rel="stylesheet">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon.png">
+    <link href="/css/style.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
 </head>
 
