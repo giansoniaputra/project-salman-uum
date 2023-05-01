66e47cd6b6e8bb36b552d479e5e0e221d67e0205
giansoniaputra <giansonia555@gmail.com>
2023-05-01 18:08:17 +0700

    Menambah Field Roles

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,9 @@
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon.png">
     <link href="/css/style.css" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700;900&display=swap"
+        rel="stylesheet">
 </head>
 
 <body class="h-100">
@@ -17,14 +19,14 @@
         <div class="container h-100">
             <div class="row justify-content-center h-100 align-items-center">
                 <div class="col-md-6">
-					
-					<div class="authincation-content">
+
+                    <div class="authincation-content">
                         <div class="row no-gutters">
                             <div class="col-xl-12">
                                 <div class="auth-form">
-									<div class="text-center mb-3">
-										<a href="index.html"><img src="images/logo-full.png" alt=""></a>
-									</div>
+                                    <div class="text-center mb-3">
+                                        <a href="index.html"><img src="images/logo-full.png" alt=""></a>
+                                    </div>
                                     <h4 class="text-center mb-4 text-white">Silahkan Buat Akun</h4>
                                     <form action="index.html">
                                         <div class="form-group">
@@ -39,12 +41,23 @@
                                             <label class="mb-1 text-white"><strong>Password</strong></label>
                                             <input type="password" class="form-control" value="Password">
                                         </div>
+                                        <div class="form-group">
+                                            <label for="roles" class="form-label">Roles</label>
+                                            <select class="form-select form-select-lg" name="roles" id="roles">
+                                                <option selected disabled>Pilih Role...</option>
+                                                <option value="SUPER ADMIN">SUPER ADMIN</option>
+                                                <option value="ADMIN">ADMIN</option>
+                                                <option value="MEMBER">MEMBER</option>
+                                            </select>
+                                        </div>
                                         <div class="text-center mt-4">
-                                            <button type="submit" class="btn bg-white text-primary btn-block">Daftar</button>
+                                            <button type="submit"
+                                                class="btn bg-white text-primary btn-block">Daftar</button>
                                         </div>
                                     </form>
                                     <div class="new-account mt-3">
-                                        <p class="text-white">Sudah Punya Akun? <a class="text-white" href="page-login.html">Masuk</a></p>
+                                        <p class="text-white">Sudah Punya Akun? <a class="text-white"
+                                                href="page-login.html">Masuk</a></p>
                                     </div>
                                 </div>
                             </div>
@@ -55,14 +68,15 @@
         </div>
     </div>
 
-<!--**********************************
+    <!--**********************************
 	Scripts
 ***********************************-->
-<!-- Required vendors -->
-<script src="./vendor/global/global.min.js"></script>
-<script src="./vendor/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
-<script src="./js/custom.min.js"></script>
-<script src="./js/deznav-init.js"></script>
+    <!-- Required vendors -->
+    <script src="./vendor/global/global.min.js"></script>
+    <script src="./vendor/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+    <script src="./js/custom.min.js"></script>
+    <script src="./js/deznav-init.js"></script>
 
 </body>
+
 </html>
