ea26cac56387decf503fdf26d99c91f98a690a0b
salfarizi <lssalman32@gmail.com>
2023-05-04 11:06:54 +0700

    nambah modal accordion

--- resources/views/pembelian/index.blade.php
+++ resources/views/pembelian/index.blade.php
@@ -43,15 +43,54 @@
         </div>
     </div>
     {{-- Modal --}}
-    <div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true" id="modal_detail">
+    <div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true" id="modal-detail">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Modal title</h5>
+                    <h5 class="modal-title">Detail Informasi</h5>
                     <button type="button" class="close" data-dismiss="modal"><span>&times;</span>
                     </button>
                 </div>
-                <div class="modal-body">Modal body text goes here.</div>
+                <div class="modal-body">
+                    <div class="col-xl-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div id="accordion-two" class="accordion accordion-primary-solid">
+                                    <div class="accordion__item">
+                                        <div class="accordion__header collapsed" data-toggle="collapse" data-target="#bordered_collapseOne"> <span class="accordion__header--text">Data Konsumen</span>
+                                            <span class="accordion__header--indicator"></span>
+                                        </div>
+                                        <div id="bordered_collapseOne" class="collapse accordion__body" data-parent="#accordion-two">
+                                            <div class="accordion__body--text">
+                                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="accordion__item">
+                                        <div class="accordion__header collapsed" data-toggle="collapse" data-target="#bordered_collapseTwo"> <span class="accordion__header--text">Data Motor</span>
+                                            <span class="accordion__header--indicator"></span>
+                                        </div>
+                                        <div id="bordered_collapseTwo" class="collapse accordion__body" data-parent="#accordion-two">
+                                            <div class="accordion__body--text">
+                                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="accordion__item">
+                                        <div class="accordion__header collapsed" data-toggle="collapse" data-target="#bordered_collapseThree"> <span class="accordion__header--text">Data Pembelian</span>
+                                            <span class="accordion__header--indicator"></span>
+                                        </div>
+                                        <div id="bordered_collapseThree" class="collapse accordion__body" data-parent="#accordion-two">
+                                            <div class="accordion__body--text">
+                                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-danger light" data-dismiss="modal">Close</button>
                     <button type="button" class="btn btn-primary">Save changes</button>
