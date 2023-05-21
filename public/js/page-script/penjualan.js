168118b9e28b56f9ce4b49d62967608f5bc7db09
salfarizi <lssalman32@gmail.com>
2023-05-21 15:59:06 +0700

    logo pembelian js

--- public/js/page-script/penjualan.js
+++ public/js/page-script/penjualan.js
@@ -468,13 +468,11 @@ $(document).ready(function () {
         $(this).removeClass("is-invalid");
         $(".jumlah_bayar").remove();
     });
-});
-//Hendler Icon Material Date Time
+    //Hendler Icon Material Date Time
 let monthBefore = $(".dtp-select-month-before .material-icons");
-monthBefore.addClass("flaticon-381-back-2 text-white");
+monthBefore.addClass("fa fa-arrow-left fa-lg text-white");
 monthBefore.removeClass("material-icons");
 monthBefore.html("");
-
 
 let yearBefore = $(".dtp-select-year-before .material-icons");
 yearBefore.addClass("fa fa-arrow-left fa-lg text-white");
@@ -496,11 +494,12 @@ yearRangeBefore.addClass("fa fa-arrow-up fa-lg text-dark");
 yearRangeBefore.removeClass("material-icons");
 yearRangeBefore.html("");
 
-
 let yearRangeAfter = $(".dtp-select-year-range.after .material-icons");
-yearRangeAfter.addClass("flaticon-381-download text-dark");
+yearRangeAfter.addClass("fa fa-arrow-down fa-lg text-dark");
 yearRangeAfter.removeClass("material-icons");
 yearRangeAfter.html("");
+});
+
 
 //Show Gambar KTP
 function previewImageKTP() {
