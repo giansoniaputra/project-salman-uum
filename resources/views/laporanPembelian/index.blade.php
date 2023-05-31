6d682a8f8a865dbe4138e5b1cc2d3be132d2c8a2
salfarizi <lssalman32@gmail.com>
2023-05-31 15:11:31 +0700

    menambahkan halaman laporan pembelian

--- resources/views/laporanPembelian/index.blade.php
+++ resources/views/laporanPembelian/index.blade.php
@@ -1,4 +1,112 @@
 @extends('layout.main')
 @section('container')
-
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title data-pelanggan">Cetak Laporan Pembelian</h4>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-12">
+                        <label class="text-label" for="laporan_pertahun">Laporan Pembelian Pertahun</label>
+                        <div class="input-group mt-2">
+                            <label class="input-group-text" for="inputGroupSelect01"><i class="bi-calendar-check-fill icon-16 text-primary"></i></label>
+                            <select class="form-select rounded-md-bottom-end rounded-md-top-end" id="inputGroupSelect04" aria-label="Example select with button addon">
+                                <option selected>Pilih Tahun</option>
+                                <option value="2023">2023</option>
+                                <option value="2022">2022</option>
+                                <option value="2021">2021</option>
+                            </select>
+                            <span class="mx-2"></span>
+                            <button class="btn btn-primary rounded-md-bottom-start rounded-md-top-start" type="button">
+                                <i data-acorn-icon="download" data-acorn-size="18"></i>
+                                PDF
+                            </button>
+                            <button class="btn btn-secondary" type="button">
+                                <i data-acorn-icon="print" data-acorn-size="18"></i>
+                                Print
+                            </button>
+                        </div>
+                    </div>
+                    <div class="col-12 mt-5">
+                        <label class="text-label" for="laporan_perbulan">Laporan Pembelian Perbulan</label>
+                        <div class="input-group mt-2">
+                            <label class="input-group-text" for="inputGroupSelect01"><i class="bi-calendar2-month-fill icon-16 text-primary"></i></label>
+                            <select class="form-select rounded-md-bottom-end rounded-md-top-end" id="inputGroupSelect04" aria-label="Example select with button addon">
+                                <option selected>Pilih Bulan</option>
+                                <option value="">Januari</option>
+                                <option value="">Februari</option>
+                                <option value="">Maret</option>
+                                <option value="">April</option>
+                                <option value="">Mei</option>
+                                <option value="">Juni</option>
+                                <option value="">Juli</option>
+                                <option value="">Agustus</option>
+                                <option value="">September</option>
+                                <option value="">Oktober</option>
+                                <option value="">Desember</option>
+                            </select>
+                            <span class="mx-2"></span>
+                            <button class="btn btn-primary rounded-md-bottom-start rounded-md-top-start" type="button">
+                                <i data-acorn-icon="download" data-acorn-size="18"></i>
+                                PDF
+                            </button>
+                            <button class="btn btn-secondary" type="button">
+                                <i data-acorn-icon="print" data-acorn-size="18"></i>
+                                Print
+                            </button>
+                        </div>
+                    </div>
+                    <div class="col-12 mt-5">
+                        <form action="/pembelianDate" method="post">
+                            @csrf
+                            <label class="text-label mb-2" for="laporan_pertanggal">Laporan Pembelian Pertanggal</label>
+                            <div class="input-daterange input-group">
+                                <input type="date" class="date form-control" name="tanggal_awal" placeholder="Tanggal Awal" />
+                                <span class="mx-2"></span>
+                                <input type="date" class="date form-control" name="tanggal_akhir" placeholder="Tanggal Akhir" />
+                                <span class="mx-2"></span>
+                                <button class="btn btn-primary rounded-md-bottom-start rounded-md-top-start" type="submit">
+                                    <i data-acorn-icon="download" data-acorn-size="18"></i>
+                                    PDF
+                                </button>
+                                <button type="button" class="btn btn-secondary">
+                                    <i data-acorn-icon="print" data-acorn-size="18"></i>
+                                    Print
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-12 mt-5">
+                        <label class="text-label" for="laporan_pertanggal">Laporan Pembelian Hari Ini</label>
+                        <div class="input-daterange input-group mt-2">
+                            <a href="/pembelianDay" target="_blank" class="btn btn-primary rounded-md-bottom-start rounded-md-top-start">
+                                <i data-acorn-icon="download" data-acorn-size="18"></i>
+                                PDF
+                            </a>
+                            <button type="button" class="btn btn-secondary">
+                                <i data-acorn-icon="print" data-acorn-size="18"></i>
+                                Print
+                            </button>
+                        </div>
+                    </div>
+                    <div class="col-12 mt-5">
+                        <label class="text-label" for="laporan_pertanggal">Laporan Pembelian Minggu Ini</label>
+                        <div class="input-daterange input-group mt-2">
+                            <a href="/pembelianWeek" class="btn btn-primary rounded-md-bottom-start rounded-md-top-start">
+                                <i data-acorn-icon="download" data-acorn-size="18"></i>
+                                PDF
+                            </a>
+                            <button type="button" class="btn btn-secondary">
+                                <i data-acorn-icon="print" data-acorn-size="18"></i>
+                                Print
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
