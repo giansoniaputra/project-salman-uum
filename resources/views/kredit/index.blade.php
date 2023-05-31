5ce5468ec3d31b988a2dd99b67d43b2c597f4b2f
salfarizi <lssalman32@gmail.com>
2023-05-31 16:34:49 +0700

    membuat halaman summary penjualan cash & kredit

--- resources/views/kredit/index.blade.php
+++ resources/views/kredit/index.blade.php
@@ -230,68 +230,93 @@
             </div>
             <div class="modal-body">
                 <div class="row">
-                    <div class="col-12">
-                        <label class="text-label" for="laporan_pertahun">Laporan Pertahun</label>
-                        <div class="input-group mt-2">
-                            <label class="input-group-text" for="inputGroupSelect01"><i class="bi-calendar-check-fill icon-16 text-primary"></i></label>
-                            <select class="form-select rounded-md-bottom-end rounded-md-top-end" id="inputGroupSelect04" aria-label="Example select with button addon">
-                                <option selected>Pilih Tahun</option>
-                                <option value="2023">2023</option>
-                                <option value="2022">2022</option>
-                                <option value="2021">2021</option>
-                            </select>
-                            <span class="mx-2"></span>
-                            <button class="btn btn-primary rounded-md-bottom-start rounded-md-top-start" type="button">
+                    <div class="col-12 mt-5">
+                        <label class="text-label" for="laporan_perbulan">Laporan Penjualan Kredit Perbulan</label>
+                        <form action="/pembelianSelectMonth" method="post">
+                            @csrf
+                            <div class="input-group mt-2">
+                                <label class="input-group-text" for="inputGroupSelect01"><i class="bi-calendar2-month-fill icon-16 text-primary"></i></label>
+                                <select name="bulan" class="form-select rounded-md-bottom-end rounded-md-top-end" id="inputGroupSelect04" aria-label="Example select with button addon" required>
+                                    <option selected disabled value="">Pilih Bulan</option>
+                                    <option value="01">Januari</option>
+                                    <option value="02">Februari</option>
+                                    <option value="03">Maret</option>
+                                    <option value="04">April</option>
+                                    <option value="05">Mei</option>
+                                    <option value="06">Juni</option>
+                                    <option value="07">Juli</option>
+                                    <option value="08">Agustus</option>
+                                    <option value="09">September</option>
+                                    <option value="10">Oktober</option>
+                                    <option value="11">November</option>
+                                    <option value="12">Desember</option>
+                                </select>
+                                <span class="mx-2"></span>
+                                <button class="btn btn-primary rounded-md-bottom-start rounded-md-top-start" type="submit">
+                                    <i data-acorn-icon="download" data-acorn-size="18"></i>
+                                    PDF
+                                </button>
+                                <button class="btn btn-secondary" type="button">
+                                    <i data-acorn-icon="print" data-acorn-size="18"></i>
+                                    Print
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-12 mt-5">
+                        <form action="/pembelianDate" method="post">
+                            @csrf
+                            <label class="text-label mb-2" for="laporan_pertanggal">Laporan Penjualan Kredit Pertanggal</label>
+                            <div class="input-daterange input-group">
+                                <input type="date" class="date form-control" name="tanggal_awal" placeholder="Tanggal Awal" required />
+                                <span class="mx-2"></span>
+                                <input type="date" class="date form-control" name="tanggal_akhir" placeholder="Tanggal Akhir" required />
+                                <span class="mx-2"></span>
+                                <button class="btn btn-primary rounded-md-bottom-start rounded-md-top-start" type="submit">
+                                    <i data-acorn-icon="download" data-acorn-size="18"></i>
+                                    PDF
+                                </button>
+                                <button type="button" class="btn btn-secondary">
+                                    <i data-acorn-icon="print" data-acorn-size="18"></i>
+                                    Print
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-12 mt-5">
+                        <label class="text-label" for="laporan_pertanggal">Laporan Penjualan Kredit Hari Ini</label>
+                        <div class="input-daterange input-group mt-2">
+                            <a href="/pembelianDay" target="_blank" class="btn btn-primary rounded-md-bottom-start rounded-md-top-start">
                                 <i data-acorn-icon="download" data-acorn-size="18"></i>
                                 PDF
-                            </button>
-                            <button class="btn btn-secondary" type="button">
+                            </a>
+                            <button type="button" class="btn btn-secondary">
                                 <i data-acorn-icon="print" data-acorn-size="18"></i>
                                 Print
                             </button>
                         </div>
                     </div>
                     <div class="col-12 mt-5">
-                        <label class="text-label" for="laporan_perbulan">Laporan Perbulan</label>
-                        <div class="input-group mt-2">
-                            <label class="input-group-text" for="inputGroupSelect01"><i class="bi-calendar2-month-fill icon-16 text-primary"></i></label>
-                            <select class="form-select rounded-md-bottom-end rounded-md-top-end" id="inputGroupSelect04" aria-label="Example select with button addon">
-                                <option selected>Pilih Bulan</option>
-                                <option value="">Januari</option>
-                                <option value="">Februari</option>
-                                <option value="">Maret</option>
-                                <option value="">April</option>
-                                <option value="">Mei</option>
-                                <option value="">Juni</option>
-                                <option value="">Juli</option>
-                                <option value="">Agustus</option>
-                                <option value="">September</option>
-                                <option value="">Oktober</option>
-                                <option value="">Desember</option>
-                            </select>
-                            <span class="mx-2"></span>
-                            <button class="btn btn-primary rounded-md-bottom-start rounded-md-top-start" type="button">
+                        <label class="text-label" for="laporan_pertanggal">Laporan Penjualan Kredit Minggu Ini</label>
+                        <div class="input-daterange input-group mt-2">
+                            <a href="/pembelianWeek" class="btn btn-primary rounded-md-bottom-start rounded-md-top-start">
                                 <i data-acorn-icon="download" data-acorn-size="18"></i>
                                 PDF
-                            </button>
-                            <button class="btn btn-secondary" type="button">
+                            </a>
+                            <button type="button" class="btn btn-secondary">
                                 <i data-acorn-icon="print" data-acorn-size="18"></i>
                                 Print
                             </button>
                         </div>
                     </div>
                     <div class="col-12 mt-5">
-                        <label class="text-label" for="laporan_pertanggal">Laporan Pertanggal</label>
-                        <div class="input-daterange input-group mt-2" id="datePickerRange">
-                            <input type="text" class="form-control" name="start" placeholder="Tanggal Awal" />
-                            <span class="mx-2"></span>
-                            <input type="text" class="form-control" name="end" placeholder="Tanggal Akhir" />
-                            <span class="mx-2"></span>
-                            <button class="btn btn-primary rounded-md-bottom-start rounded-md-top-start" type="button">
+                        <label class="text-label" for="laporan_pertanggal">Laporan Penjualan Kredit Bulan Ini</label>
+                        <div class="input-daterange input-group mt-2">
+                            <a href="/pembelianMonth" class="btn btn-primary rounded-md-bottom-start rounded-md-top-start">
                                 <i data-acorn-icon="download" data-acorn-size="18"></i>
                                 PDF
-                            </button>
-                            <button class="btn btn-secondary" type="button">
+                            </a>
+                            <button type="button" class="btn btn-secondary">
                                 <i data-acorn-icon="print" data-acorn-size="18"></i>
                                 Print
                             </button>
