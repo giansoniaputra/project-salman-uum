2d32f1c041491b73912048649628b41c8a3af313
salfarizi <lssalman32@gmail.com>
2023-06-02 10:44:39 +0700

    menambahkan cropper dihalaman profile

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -42,8 +42,17 @@
                         </div>
                     </div>
                     <div class="input-group mb-3">
-                        <input type="file" class="form-control" id="inputGroupFile02" />
+                        <input type="file" class="form-control @error('photo_pria') is-invalid @enderror" name="photo_pria" id="photo_pria">
                         <label class="input-group-text" for="inputGroupFile02">Upload</label>
+                        <input type="hidden" name="fotoPria" id="fotoPria" value="{{ old('fotoPria') }}">
+                        @error('photo_pria')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
+                    <div class="col-md-12" id="show-foto-pria">
+                        {{-- <img src="{{ old('fotoPria', '/storage/post-images/mempelai/'.$data->photo_pria) }}" alt="" class="img-fluid show-foto-pria" width="200px"> --}}
                     </div>
                     <div class="d-grid gap-2 d-md-flex justify-content-md-end">
                         <button class="btn btn-primary me-md-2" type="button">Edit Data Profile</button>
@@ -53,4 +62,27 @@
         </div>
     </div>
 </div>
+<!-- Modal  Cropper -->
+<div class="modal fade" id="modal-cropper" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Crop Foto</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col">
+                        <img id="image" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                <button type="button" class="btn btn-primary crop_pria">Crop</button>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="/page-script/photo-profile.js"></script>
 @endsection
