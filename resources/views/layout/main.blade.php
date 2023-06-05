ffa00eb30ddb6659410bfedd1d4de225ddc0a0c3
giansoniaputra <giansonia555@gmail.com>
2023-06-05 10:23:19 +0700

    Membetulkan main

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -84,7 +84,7 @@
                 @if(Setting::count() > 0)
                 <h6 class="cta-4 mt-1 text-white">{{ ucwords(strtolower(Setting::first()->nama_toko)) }}</h6>
                 @else
-                <h6 class="cta-4 mt-1 text-white">{{ SISMAC }}</h6>
+                <h6 class="cta-4 mt-1 text-white">SISMAC</h6>
                 @endif
 
                 <!-- Logo End -->
