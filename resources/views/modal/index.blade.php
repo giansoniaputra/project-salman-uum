982175fd7abf3bcaefd044b642b4fbd287928871
giansoniaputra <giansonia555@gmail.com>
2023-07-02 15:36:44 +0700

    revisi tgl 2 juli

--- resources/views/modal/index.blade.php
+++ resources/views/modal/index.blade.php
@@ -152,7 +152,7 @@
                             <div class="d-flex align-items-center lh-1-25">Modal + Laba Cash</div>
                         </div>
                         <div class="col-12 col-xl-auto">
-                            <div class="cta-2 text-primary" id="refresh-modal">{{ rupiah($data->modal + $laba) }}</div>
+                            <div class="cta-2 text-primary" id="refresh-modal">{{ rupiah($data->modal + $laba + $laba_kredit) }}</div>
                         </div>
                     </div>
                 </div>
@@ -194,7 +194,7 @@
                             <div class="d-flex align-items-center lh-1-25">Saldo Bank</div>
                         </div>
                         <div class="col-12 col-xl-auto">
-                            <div class="cta-2 text-primary" id="refresh-modal">{{ rupiah($sisa_bank) }}</div>
+                            <div class="cta-2 text-primary" id="refresh-modal">{{ rupiah($sisa_bank + $laba_kredit) }}</div>
                         </div>
                     </div>
                 </div>
